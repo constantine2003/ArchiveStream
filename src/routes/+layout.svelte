a4bff48294f1f9ed0ea062dad8e5134aab52e2d0
Daniel Montesclaros <147793265+constantine2003@users.noreply.github.com>
2026-03-20 17:25:56 +0800

    Add archivestream.svg and update favicon import
    Add a new SVG asset at src/lib/assets/archivestream.svg and update src/routes/+layout.svelte to import this SVG as the favicon instead of the previous archivestreampng.png. This switches the app to a scalable SVG favicon and includes the SVG graphic file in the assets.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/archivestreampng.png';
+	import favicon from '$lib/assets/archivestream.svg';
 
 	let { children } = $props();
 </script>
